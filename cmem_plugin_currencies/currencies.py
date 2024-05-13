db3eeccf2d913a8eaf6f0259012d250e90c3f84f
Annika Bessing <annika.bessing@eccenca.com>
2024-05-13 09:28:15 +0200

    Version2

--- cmem_plugin_currencies/currencies.py
+++ cmem_plugin_currencies/currencies.py
@@ -1,6 +1,5 @@
 """currency converter plugin module"""
 from collections.abc import Sequence
-
 import requests
 from cmem_plugin_base.dataintegration.description import (
     Plugin,
@@ -11,39 +10,62 @@ from cmem_plugin_base.dataintegration.plugins import TransformPlugin
 
 @Plugin(
     label="Currency Converter",
-    description="Converts Currency"
+    description="Converts currencies"
     " from one to another."
-    " Please use point as decimal separator "
-    " and currency identifier as Currencies (e.g. EUR)",
+    " Please use currency identifier e.g. EUR",
     documentation="""
-This Converter allows you to convert currencies from one currency to another.
+This converter plugin allows you to convert currencies from one currency to another.
 """,
     parameters=[
         PluginParameter(
             name="to_currency",
             label="Target Currency",
-            description="currency identifier (e.g. USD).",
+            description="Enter the currency code you want to convert to (e.g.USD).",
         ),
         PluginParameter(
             name="from_currency",
             label="Source Currency",
-            description="currency identifier (e.g. EUR).",
+            description="Enter the currency code you want to convert from (e.g. EUR).",
+        ),
+        PluginParameter(
+            name="historic",
+            label="Historic Rate",
+            description="Set date (e.g.YYYY-MM-DD) to convert currencies based on historic rates.",
         ),
     ],
 )
 class CurrenciesConverter(TransformPlugin):
     """Currency Converter Plugin"""
 
-    def __init__(self, to_currency: str, from_currency: str):
-        self.to_currency = to_currency
-        self.from_currency = from_currency
-        """API access"""
-        base_url = "https://api.frankfurter.app/latest"
-        params = {"from": self.from_currency, "to": self.to_currency}
-        response = requests.get(base_url, params=params, timeout=10)
-        data = response.json()
-        self.exchange_rate = float(data["rates"][self.to_currency])
+    def __init__(self, to_currency: str, from_currency: str, historic: str):
+        self.historic = historic
+
+        """Currency Check"""
+        base_url = "https://api.frankfurter.app/"
+        response = requests.get(base_url+"currencies", timeout=10)
+
+        if to_currency.upper() in response.json() and from_currency.upper() in response.json():
+            self.to_currency = to_currency.upper()
+            self.from_currency = from_currency.upper()
+
+            """API access, Historic or latest Rate"""
+            if len(historic) > 0:
+                url = base_url+historic
+            else:
+                url = base_url+"latest"
+
+            params = {"from": self.from_currency, "to": self.to_currency}
+            response = requests.get(url, params=params, timeout=10)
+            data = response.json()
+            self.exchange_rate = float(data["rates"][self.to_currency])
+        else:
+            self.to_currency = ""
+            self.from_currency = ""
 
     def transform(self, inputs: Sequence[str]) -> Sequence[str]:
         """Do the actual transformation of values"""
-        return [str(self.exchange_rate * float(value)) for value in inputs]
+        if self.to_currency != "" and self.from_currency != "":
+            for value in inputs:
+                return [str(self.exchange_rate * float(_)) for _ in value]
+        else:
+            return "Currency not listed"
--- tests/test_currencies.py
+++ tests/test_currencies.py
@@ -4,7 +4,7 @@ from cmem_plugin_currencies.currencies import CurrenciesConverter
 
 def test_init() -> None:
     """Test Initialization"""
-    plugin = CurrenciesConverter(from_currency="EUR", to_currency="USD")
+    plugin = CurrenciesConverter(from_currency="EUR", to_currency="USD", historic="")
     eur_values = ["1", "2"]
     usd_values = plugin.transform(inputs=eur_values)
     assert len(usd_values) == len(eur_values)
